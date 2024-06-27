d80c8b740f7c1b0c554e7cd5f41f87584c1a1485
ArsyaMC <121472019+arsyamc@users.noreply.github.com>
2024-06-27 14:30:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(express.static('image'));
 
 app.listen(3000, () => {
-  console.log('server started');
+  console.log('server started!!');
 });
 
 app.use('/discord', routerBot);
